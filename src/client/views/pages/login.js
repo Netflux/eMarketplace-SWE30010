3de176921a5764ab54fb50f420b2b8a77dcff318
svencrox <4232909@students.swinburne.edu.my>
2017-11-05 23:52:22 +0800

    login and signup tweaks

--- src/client/views/pages/login.js
+++ src/client/views/pages/login.js
@@ -1,10 +1,13 @@
 import './css/LoginPage.css'
 
+import { fetchUsers } from 'client/logic/actions/users'
+
 const login = {
 	templateUrl: 'templates/pages/LoginPage.html',
 	controller: ['$store', '$http', '$location', function($store, $http, $location){
 		this.$onDestroy = $store.subscribe(state =>{
 		
+		//login function
 		this.login = function(){
 			
 			this.user ={
@@ -12,26 +15,33 @@ const login = {
 				password: this.password
 			}
 			console.log(this.user)
+			//fetchUsers($store, $http)
 			
 			$http({withCredentials: true, method:'post', url:'api/users/login', data: this.user, headers: {'Content-Type': 'application/json'}})
 				.then(function successCallback(response) {
 					alert("Login Success")
+					//fetchUsers($store, $http)
 					$location.path("/account")
 			  }, function errorCallback(response) {
 					alert("Can't do that right now. Try again later")
+					console.log(response.data.id)
 			  })
+			console.log(this.user.username)
 			
 		}
 		
+		//logout function
 		this.logout = function(){
 			$http({withCredentials: true, method:'post', url:'api/users/logout'})
 				.then(function successCallback(response) {
-					alert("LOGOUT Success")
+					alert("Logout Success")
 			  }, function errorCallback(response) {
 					alert("Can't logout right now. Try again later")
 			  })
 		}
 		})
+		
+		fetchUsers($store, $http)
 	}]
 }
 
--- src/client/views/pages/signup.js
+++ src/client/views/pages/signup.js
@@ -17,50 +17,21 @@ const signup = {
 			}
 			console.log(this.user)
 			$http({
-				method: 'POST',
+				method: 'post',
 				url: 'api/users/register',
 				data: this.user,
 				headers: {
 					'Content-Type': 'application/json'
 				}
-			})
-				.then(function successCallback(response) {
-					alert("signup Success")
-					$location.path("/login")
-				  }, function errorCallback(response) {
-					alert("Can't do that right now. Try again later")
-					alert(response.data)
-				  })
+			}).then(function successCallback(response) {
+				alert("signup Success")
+				$location.path("/login")
+			  }, function errorCallback(response) {
+				alert("Can't do that right now. Try again later")
+				alert(response.data)
+			  })
 		}
-	}],
-	directive: ['passwordVerify', function() {
-		return {
-			restrict:'A',
-			require:'?ngModel',
-			link: function(scope, elem, attrs, ngModel) {
-				// do nothing if no ng-model
-				if (!ngModel) return
-
-				this.$watch(attrs.ngModel, function() {
-					validate()
-				})
-
-				// observe the other value and re-validate on change
-				attrs.$observe('passwordVerify', function() {
-					validate()
-				})
-
-				var validate = function() {
-					// values
-					var val1 = ngModel.$viewValue
-					var val2 = attrs.passwordVerify
-
-					// set validity
-					ngModel.$setValidity('passwordVerify', val1 === val2)
-				}
-			}
-		}
-	}]	
+	}]
 }
 
 export default signup
