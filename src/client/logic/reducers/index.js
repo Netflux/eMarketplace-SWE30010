716bb380aaa7b3dab7e226a8ac2e0924f044e77a
victorlyc97 <victorlyc97@gmail.com>
2017-11-07 02:27:44 +0800

    Edit product review section

--- src/client/logic/reducers/index.js
+++ src/client/logic/reducers/index.js
@@ -3,15 +3,13 @@ import productsReducer from './products'
 import usersReducer from './users'
 import basketsReducer from './basket'
 import ordersReducer from './orders'
-import ordersReviews from './reviews'
 
 const rootReducer = (state, action) => ({
 	categories: categoriesReducer(state.categories, action),
 	products: productsReducer(state.products, action),
 	users: usersReducer(state.users, action),
 	basket: basketsReducer(state.basket, action),
-	orders: ordersReducer(state.orders, action),
-    reviews: ordersReviews(state.reviews, action)
+	orders: ordersReducer(state.orders, action)
 })
 
 export default rootReducer
--- src/client/views/pages/product.js
+++ src/client/views/pages/product.js
@@ -4,37 +4,84 @@ import { fetchCategories } from 'client/logic/actions/categories'
 import { fetchProducts } from 'client/logic/actions/products'
 import { fetchUsers } from 'client/logic/actions/users'
 import { fetchBasket } from 'client/logic/actions/basket'
-import { fetchReviews } from 'client/logic/actions/reviews'
 
 const product = {
 	templateUrl: 'templates/pages/ProductPage.html',
 	controller:['$store','$stateParams','$http', function($store, $stateParams, $http){
-		this.$onDestroy = $store.subscribe(state => {
-			this.products = state.products
-			this.categories = state.categories
-			this.users = state.users
-            this.reviews = state.reviews
+        const ctrl = this
+        ctrl.addBasket = function () {
+            if (ctrl.productqty <= ctrl.product.stock){
+                const basket = {
+                    productKey: ctrl.product.productKey,
+                    quantity: ctrl.productqty
+                }
+
+                $http({ withCredentials: true, method: 'post', url:'/api/basket', data: basket })
+                    .then(function sucessCallback(){
+                        alert('Successfully added to basket')
+                        fetchBasket($store,$http)
+                    }, function errorCallback(){
+                        alert('You must be logged in to buy a product.')
+                    })
+            }
+            else {
+                alert ('Not enough stock available for purchase')
+            }
+        }
+
+        ctrl.addReview = function() {  
+            const review = {
+                userId: 2,
+                productKey: ctrl.product.productKey,
+                title: ctrl.title,
+                rating: ctrl.rating,
+                description: ctrl.description
+            }
+
+            $http({ withCredentials: true, method: 'post', url:`/api/products/${ctrl.product.productKey}/reviews`, data: review })
+                    .then(function sucessCallback(){
+                        alert('Successfully added review')
+                        ctrl.fetchReviews()
+                    }, function errorCallback(){
+                        alert('You must be logged to post review.')
+                    })
+        }
+
+        ctrl.fetchReviews = function() {
+            if (ctrl.product) {
+                $http.get(`/api/products/${ctrl.product.productKey}/reviews`)
+                    .then(function success(response) {
+                        ctrl.reviews = response.data.data
+                    }, function failure() {
+                        ctrl.reviews = []
+                    })
+            }
+        }
+        
+		ctrl.$onDestroy = $store.subscribe(state => {
+			ctrl.products = state.products
+			ctrl.categories = state.categories
+			ctrl.users = state.users
             
 			// Check local cache
-			this.product = this.products.items.find(i => i.productKey === $stateParams.productKey)
-			this.category = this.product ? this.categories.items.find(i => i.categoryId === this.product.categoryId) : undefined
-			this.user = this.product ? this.users.items.find(i => i.userId === this.product.userId) : undefined
-            this.review = this.product ? this.reviews.items.find(i => i.productKey === this.product.productKey) : undefined
+			ctrl.product = ctrl.products.items.find(i => i.productKey === $stateParams.productKey)
+			ctrl.category = ctrl.product ? ctrl.categories.items.find(i => i.categoryId === ctrl.product.categoryId) : undefined
+			ctrl.user = ctrl.product ? ctrl.users.items.find(i => i.userId === ctrl.product.userId) : undefined
             
 			// Populate breadcrumbs
-			this.crumbs = [{
+			ctrl.crumbs = [{
 				title:'Category',
 				url: 'category'
 			}, {
-				title: this.category ? this.category.title : 'Category Title',
-				url: this.category ? `products({ categoryId: ${this.category.categoryId} })` : ''
+				title: ctrl.category ? ctrl.category.title : 'Category Title',
+				url: ctrl.category ? `products({ categoryId: ${ctrl.category.categoryId} })` : ''
 			}, {
-				title: this.product ? this.product.title : 'Product Title',
-				url: this.product ? `product({ productKey: '${this.product.productKey}' })` : ''
+				title: ctrl.product ? ctrl.product.title : 'Product Title',
+				url: ctrl.product ? `product({ productKey: '${ctrl.product.productKey}' })` : ''
 			}]
 
 			// Generating random product
-			this.productRandomization = []
+			ctrl.productRandomization = []
 			var i = 0
 			function search(nameKey, myArray){
 				for (var i=0; i < myArray.length; i++) {
@@ -47,56 +94,20 @@ const product = {
 			while(i < 4){
 				var generatenumber = Math.floor((Math.random() * state.products.items.length) + 1)
 				var resultObject = search(generatenumber, state.products.items)
-				if(search(generatenumber,this.productRandomization) != resultObject){
-					this.productRandomization.push(resultObject)
+				if(search(generatenumber,ctrl.productRandomization) != resultObject){
+					ctrl.productRandomization.push(resultObject)
 				}
 				i++
 			}
             
-			this.addBasket = function () {
-				if (this.productqty <= this.product.stock){
-					this.basket = {
-						productKey: this.product.productKey,
-						quantity: this.productqty
-					}
-
-					$http({ withCredentials: true, method: 'post', url:'/api/basket', data: this.basket })
-						.then(function sucessCallback(){
-							alert('Successfully added to basket')
-							fetchBasket($store,$http)
-						}, function errorCallback(){
-							alert('You must be logged in to buy a product.')
-						})
-				}
-				else {
-					alert ('Not enough stock available for purchase')
-				}
-			}
-            
-            this.addReview = function() {  
-                this.review = {
-                    userId: 2,
-                    productKey: this.product.productKey,
-                    title: this.title,
-                    rating: this.rating,
-                    description: this.description
-                }
-                
-                $http({ withCredentials: true, method: 'post', url:`/api/products/${this.product.productKey}/reviews`, data: this.review })
-						.then(function sucessCallback(){
-							alert('Successfully added review')
-						}, function errorCallback(){
-							alert('You must be logged to post review.')
-						})
-            }
+            ctrl.fetchReviews()
 		})
-
+        
 		// In cache not found, fetch from server
 		fetchCategories($store, $http)
 		fetchProducts($store, $http)
 		fetchUsers($store, $http)
 		fetchBasket($store,$http)
-        fetchReviews($store, $http)
 	}]
 }
 
