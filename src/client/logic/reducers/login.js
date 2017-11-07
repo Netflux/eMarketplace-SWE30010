682adf6f2a6e84b51df4f2adf1be4de7ee26a210
svencrox <4232909@students.swinburne.edu.my>
2017-11-07 23:09:32 +0800

    login reducer update

--- src/client/logic/reducers/login.js
+++ src/client/logic/reducers/login.js
@@ -1,31 +1,13 @@
 import * as actions from 'client/logic/actions/login'
 
-const loginReducer = (state = {
-	lastFetched: 0,
-	isFetching: false,
-	items: []
-}, action) => {
+const loginReducer = (state = {}, action) => {
 	switch (action.type) {
-	case actions.FETCH_LOGIN:
-		return {
-			...state,
-			isFetching: true
-		}
 	case actions.RECEIVE_LOGIN: {
-		const items = state.items.filter(i => action.data.filter(j => i.userId === j.userId).length === 0)
 		return {
 			...state,
-			lastFetched: Date.now(),
-			isFetching: false,
-			items: [...items, ...action.data]
-
+			user: action.data
 		}
 	}
-	case actions.RECEIVE_LOGIN_ERROR:
-		return {
-			...state,
-			isFetching: false
-		}
 	default:
 		return state
 	}
--- src/client/views/pages/login.js
+++ src/client/views/pages/login.js
@@ -6,37 +6,34 @@ const login = {
 	templateUrl: 'templates/pages/LoginPage.html',
 	controller: ['$store', '$http', '$location', function($store, $http, $location){
 		this.$onDestroy = $store.subscribe(state =>{
-		
-		//login function
-		this.login = function(){
-			
-			this.user ={
-				username: this.username,
-				password: this.password
+			//login function	
+			this.login = function(){
+				this.user ={
+					username: this.username,
+					password: this.password
+				}
+				//console.log(this.user)
+				$http({withCredentials: true, method:'post', url:'api/users/login', data: this.user, headers: {'Content-Type': 'application/json'}})
+					.then(function successCallback(response) {
+						alert("Login Success")
+						$store.update({data: response.data.data, type: 'RECEIVE_LOGIN'})
+						//console.log($store.login)
+						$location.path("/account")
+				  }, function errorCallback(response) {
+						alert("Can't do that right now. Try again later")
+						//console.log(response.data.id)
+				  })
 			}
-			console.log(this.user)
-			
-			$http({withCredentials: true, method:'post', url:'api/users/login', data: this.user, headers: {'Content-Type': 'application/json'}})
-				.then(function successCallback(response) {
-					alert("Login Success")
-					$location.path("/account")
-			  }, function errorCallback(response) {
-					alert("Can't do that right now. Try again later")
-					console.log(response.data.id)
-			  })			
-		}
-		
-		//logout function
-		this.logout = function(){
-			$http({withCredentials: true, method:'post', url:'api/users/logout'})
-				.then(function successCallback(response) {
-					alert("Logout Success")
-			  }, function errorCallback(response) {
-					alert("Can't logout right now. Try again later")
-			  })
-		}
+			//logout function
+			this.logout = function(){
+				$http({withCredentials: true, method:'post', url:'api/users/logout'})
+					.then(function successCallback(response) {
+						alert("Logout Success")
+				  }, function errorCallback(response) {
+						alert("Can't logout right now. Try again later")
+				  })
+			}
 		})
-		
 		fetchUsers($store, $http)
 	}]
 }
