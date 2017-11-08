1ba114d80ee55aa05eb1815f0a21e13ce5a44ef4
Netflux <netfluxer@gmail.com>
2017-11-08 10:11:42 +0800

    Update NavBar links based on login status

--- src/client/views/components/navbar.js
+++ src/client/views/components/navbar.js
@@ -1,7 +1,18 @@
 import './css/NavBar.css'
 
+import { fetchLogout } from 'client/logic/actions/account'
+
 const navbar = {
-	templateUrl: 'templates/components/NavBar.html'
+	templateUrl: 'templates/components/NavBar.html',
+	controller: ['$store', '$http', function($store, $http) {
+		const ctrl = this
+		ctrl.$onDestroy = $store.subscribe(state => {
+			ctrl.isLoggedIn = state.account.isLoggedIn
+		})
+		ctrl.logout = function() {
+			fetchLogout($store, $http)
+		}
+	}]
 }
 
 export default navbar
